e51c0dffdd0147f1fe2a26e80f7da165911ff0cf
sinros01 <69473399+sinros01@users.noreply.github.com>
2020-09-01 22:35:52 +0200

    .

--- Sindre Rosenlind-weekly 1-task 5/Sindre Rosenlind-weekly 1-task 5.cpp
+++ Sindre Rosenlind-weekly 1-task 5/Sindre Rosenlind-weekly 1-task 5.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
 int main() {
-	std::cout << "       1     2     3     4     5     6            \n";
-	std::cout << "      ---   ---   ---   ---   ---   ---           \n";
-	std::cout << " A  |  *  |  *  |  *  |  *  |  *	|  *  |  A      \n";
-	std::cout << "      ---   ---   ---   ---   ---   ---           \n";
-	std::cout << " B  |  *	|  *  |  *  |  *  |  *  |  *  |  B      \n";
-	std::cout << "      ---   ---   ---   ---   ---   ---           \n";
-	std::cout << " C  |  *  |  *  |  *  |  *  |  *  |  *  |  C      \n";
-	std::cout << "      ---   ---   ---   ---   ---   ---           \n";
-	std::cout << " D  |  *  |  *  |  *  |  *  |  *  |  *  |  D      \n";
-	std::cout << "      ---   ---   ---   ---   ---   ---           \n";
-	std::cout << "       1     2     3     4     5     6            \n";
+	std::cout << "       1     2     3     4     5     6            " << std::endl;
+	std::cout << "      ---   ---   ---   ---   ---   ---           " << std::endl;
+	std::cout << " A  |  *  |  *  |  *  |  *  |  *	|  *  |  A      " << std::endl;
+	std::cout << "      ---   ---   ---   ---   ---   ---           " << std::endl;
+	std::cout << " B  |  *	|  *  |  *  |  *  |  *  |  *  |  B      " << std::endl;
+	std::cout << "      ---   ---   ---   ---   ---   ---           " << std::endl;
+	std::cout << " C  |  *  |  *  |  *  |  *  |  *  |  *  |  C      " << std::endl;
+	std::cout << "      ---   ---   ---   ---   ---   ---           " << std::endl;
+	std::cout << " D  |  *  |  *  |  *  |  *  |  *  |  *  |  D      " << std::endl;
+	std::cout << "      ---   ---   ---   ---   ---   ---           " << std::endl;
+	std::cout << "       1     2     3     4     5     6            " << std::endl;
 }
