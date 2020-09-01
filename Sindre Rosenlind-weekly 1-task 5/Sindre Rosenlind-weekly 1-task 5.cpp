005be630323f0a7bd1a790c34ef4168cd198d49b
sinros01 <69473399+sinros01@users.noreply.github.com>
2020-09-01 22:49:05 +0200

    fixed verson

--- Sindre Rosenlind-weekly 1-task 5/Sindre Rosenlind-weekly 1-task 5.cpp
+++ Sindre Rosenlind-weekly 1-task 5/Sindre Rosenlind-weekly 1-task 5.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
+
 int main() {
 	std::cout << "       1     2     3     4     5     6            " << std::endl;
 	std::cout << "      ---   ---   ---   ---   ---   ---           " << std::endl;
-	std::cout << " A  |  *  |  *  |  *  |  *  |  *	|  *  |  A      " << std::endl;
+	std::cout << " A  |  *  |  *  |  *  |  *  |  *  |  *  |  A      " << std::endl;
 	std::cout << "      ---   ---   ---   ---   ---   ---           " << std::endl;
-	std::cout << " B  |  *	|  *  |  *  |  *  |  *  |  *  |  B      " << std::endl;
+	std::cout << " B  |  *  |  *  |  *  |  *  |  *  |  *  |  B      " << std::endl;
 	std::cout << "      ---   ---   ---   ---   ---   ---           " << std::endl;
 	std::cout << " C  |  *  |  *  |  *  |  *  |  *  |  *  |  C      " << std::endl;
 	std::cout << "      ---   ---   ---   ---   ---   ---           " << std::endl;
